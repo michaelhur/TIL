b912b6a7ed722e7161cf6cb03b1e79613ece104a
michaelhur <jhhur@uwaterloo.ca>
2022-01-18 00:54:41 +0900

    fixed inputRef

---
+++ react/react-webgame/lecture/05_wordRelay_hooks/WordRelay.jsx
@@ -0,0 +1,44 @@
+const React = require('react');
+const { useState } = React;
+
+const WordRelay = () => {
+    const [word, setWord] = useState('클래식');
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+    const inputRef = React.useRef(null);  // onInputRef 는 없음
+
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+        if (word[word.length - 1] === value[0]){
+            setResult('딩동댕!');
+            setWord(value);
+            setValue('');
+            inputRef.current.focus();
+        } else {
+            setResult('땡!');
+            setValue('');
+            inputRef.current.focus();
+        }
+    };
+
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
+    };
+
+    return (
+        <>
+            <div>{ word }</div>
+            <form onSubmit={ onSubmitForm }>
+                <input
+                    ref={ inputRef }
+                    value = { value }
+                    onChange = { onChangeInput }>
+                </input>
+                <button>입력!!</button>
+            </form>
+            <div>{ result }</div>
+        </>
+    )
+}
+
+module.exports = WordRelay;

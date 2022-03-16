f62a2f3865ce0e98e99d0fea0d92a901fc3e24ac
michaelhur <jhhur@uwaterloo.ca>
2022-03-16 23:41:25 +0900

    UPDATE: Lotto using class

--- react/react-webgame/lecture/16_Lotto/Lotto.jsx
+++ react/react-webgame/lecture/16_Lotto/Lotto.jsx
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
 
+function getWinNumbers() {
+    console.log('getWinNumbers');
+    const candidate = Array(45).fill().map((v,i) => i + 1);
+    const shuffle = [];
+    while (candidate.length > 0) {
+        shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+    }
+    const bonusNumber = shuffle[shuffle.length - 1];
+    const winNumbers = shuffle.splice(0, 6).sort((p,c) => p - c);
+    return [... winNumbers, bonusNumber];
+}
+
 class Lotto extends Component{
     state = {
+        winNumbers: getWinNumbers(),
+        winBalls: [],
+        bonus: null,
+        redo: false,
+    };
+
+    onClickRedo = () => {
 
     }
 
     render() {
+        const { winBalls, bonus, redo } = this.state
         return(
             <>
+                <div>당첨 숫자</div>
+                <div id="결과창">
+                    {winBalls.map((v) => <Ball key={v} number={v})}
+                </div>
+                <div>보너스!</div>
+                { bonus && <Ball number={bonus} /> }
+                <button onClick={redo ? onClickRedo: () => {}}>한 번 더!</button>
             </>
         )
     }

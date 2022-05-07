69db70dbb5c47ca8525ac2f70bc6953a2e66cc87
michaelhur <jhhur@uwaterloo.ca>
2022-05-08 01:51:26 +0900

    ADD: overloading in typescript

---
+++ typescript/blockchain/3-1-overloading.ts
@@ -0,0 +1,25 @@
+// type Add = (a:number, b:number) => number; // this is a call signature of a function
+
+// or we can do:
+type Add = {
+    (a: number, b: number): number
+    (a: number, b: string): number
+}
+
+const add: Add = (a, b) => {
+    if (typeof b === 'string')  return a;
+    return a+b;
+} // this automatically infers that a and b are number
+
+// We can also have call signature with different number of parameters
+type Addition = {
+    (a: number, b: number): number
+    (a: number, b: number, c?: number): number
+}
+
+// typescript is complaining
+// so we have to specify that c is optional -> c?: number
+const addition:Addition = (a, b, c?:number) => {
+    if (c) return a + b + c
+    return a + b
+}

6d0348689c34454f0a6fbe196f10905bf76c7fa1
michaelhur <jhhur@uwaterloo.ca>
2022-05-30 23:24:39 +0900

    ADD: habit component

--- react/dream-coding/react-basic/habit-tracker/src/app.jsx
+++ react/dream-coding/react-basic/habit-tracker/src/app.jsx
@@ -1,16 +1,8 @@
 import './app.css';
+import Habit from "./components/habit";
 
 function App() {
-  const name = "mike";
-    return (
-        <>
-            <h1>hello</h1>
-            {name && <h1> hello {name}!</h1>}
-            {
-                [1,2,3].map(item => (<h1>item</h1>))
-            }
-        </>
-    )
+    return <Habit />
 }
 
 export default App;

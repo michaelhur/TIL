fe9dc0abb65e0fae7d0367349e8305928625b70c
michaelhur <jhhur@uwaterloo.ca>
2022-06-02 23:14:12 +0900

    ADD: navbar

--- react/dream-coding/react-basic/habit-tracker/src/components/navbar.jsx
+++ react/dream-coding/react-basic/habit-tracker/src/components/navbar.jsx
@@ -2,10 +2,9 @@ import React, {Component} from 'react';
 
 class Navbar extends Component {
 
-
     render() {
         return (
-            <nav>
+            <nav className="navbar">
                 <i className="navbar-log fas fa-leaf"></i>
                 <span>Habit Tracker</span>
                 <span className="navbar-count">{this.props.totalCount}</span>

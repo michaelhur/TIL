aacd203155d71b9bd5e600939beb9e57ced6869a
michaelhur <jhhur@uwaterloo.ca>
2022-06-04 00:43:13 +0900

    UPDATE: pure component on add form

--- react/dream-coding/react-basic/habit-tracker/src/components/navbar.jsx
+++ react/dream-coding/react-basic/habit-tracker/src/components/navbar.jsx
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 class Navbar extends Component {
 
     render() {
+        console.log("navbar");
         return (
             <nav className="navbar">
                 <i className="navbar-log fas fa-leaf"></i>

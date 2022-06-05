2c34ca759d50d4bbcff783582d30a5a40da5ddc6
michaelhur <jhhur@uwaterloo.ca>
2022-06-06 00:06:30 +0900

    UPDATE: videos components

--- react/dream-coding/react-basic/youtube-clone/src/app.jsx
+++ react/dream-coding/react-basic/youtube-clone/src/app.jsx
@@ -1,8 +1,27 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import VideoList from "./components/video_list/video_list";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch(
+        "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCmBr_x4gehHuQSL3d2OGB67vAeDA8QCVU",
+        requestOptions
+    )
+        .then(response => response.json())
+        .then(result => setVideos(result.items))
+        .catch(error => console.log('error', error));
+  }, []);
+
   return (
-      <h1>Hello :)</h1>
+      <VideoList videos={videos} />
   );
 }
 

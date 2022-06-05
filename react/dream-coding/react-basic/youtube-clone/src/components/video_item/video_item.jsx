0fe3397fd029358f888b5f0cea2ff9f2ea8fe0c5
michaelhur <jhhur@uwaterloo.ca>
2022-06-06 00:13:53 +0900

    UPDATE: commented out thumbnail

--- react/dream-coding/react-basic/youtube-clone/src/components/video_item/video_item.jsx
+++ react/dream-coding/react-basic/youtube-clone/src/components/video_item/video_item.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const VideoItem = (props) => {
     return(
+        <>
+        {/*<img src={props.video.snippet.thumbnails.default.url} />*/}
         <h1>{props.video.snippet.title}</h1>
+        </>
     )
 }
 

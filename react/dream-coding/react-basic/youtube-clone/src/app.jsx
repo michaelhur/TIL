6fcfc941cab015325259a07802760c8dbbfe7ac8
michaelhur <jhhur@uwaterloo.ca>
2022-06-06 22:15:18 +0900

    ADD: search header ui:

--- react/dream-coding/react-basic/youtube-clone/src/app.jsx
+++ react/dream-coding/react-basic/youtube-clone/src/app.jsx
@@ -1,28 +1,32 @@
-import './App.css';
+import styles from './app.module.css';
 import { useState, useEffect } from 'react';
 import VideoList from "./components/video_list/video_list";
+import SearchHeader from "./components/search_header/search_header";
 
 function App() {
-  const [videos, setVideos] = useState([]);
+    const [videos, setVideos] = useState([]);
 
-  useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
+    useEffect(() => {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
 
-    fetch(
-        "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCmBr_x4gehHuQSL3d2OGB67vAeDA8QCVU",
-        requestOptions
-    )
-        .then(response => response.json())
-        .then(result => setVideos(result.items))
-        .catch(error => console.log('error', error));
-  }, []);
+        fetch(
+            "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCmBr_x4gehHuQSL3d2OGB67vAeDA8QCVU",
+            requestOptions
+        )
+            .then(response => response.json())
+            .then(result => setVideos(result.items))
+            .catch(error => console.log('error', error));
+    }, []);
 
-  return (
-      <VideoList videos={videos} />
-  );
+    return (
+        <div className={styles.app}>
+            <SearchHeader />
+            <VideoList videos={videos} />
+        </div>
+    );
 }
 
 export default App;

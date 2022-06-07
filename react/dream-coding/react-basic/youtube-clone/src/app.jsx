1925dde733eac2895985a0b4149eb584110d004d
michaelhur <jhhur@uwaterloo.ca>
2022-06-07 23:29:07 +0900

    UPDATE: refactoring app.jsx

--- react/dream-coding/react-basic/youtube-clone/src/app.jsx
+++ react/dream-coding/react-basic/youtube-clone/src/app.jsx
@@ -3,36 +3,22 @@ import { useState, useEffect } from 'react';
 import VideoList from "./components/video_list/video_list";
 import SearchHeader from "./components/search_header/search_header";
 
-function App() {
+
+function App({ youtube }) {
     const [videos, setVideos] = useState([]);
 
-    const onSearch = (value) => {
-        const requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-        };
-
-        fetch(
-            `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${value}&type=video&key=AIzaSyCmBr_x4gehHuQSL3d2OGB67vAeDA8QCVU`, requestOptions)
-            .then(response => response.json())
-            .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
-            .then(items => setVideos(items))
-            .catch(error => console.log('error', error));
+    const onSearch = (query) => {
+        youtube
+            .search(query)
+            .then(videos => setVideos(videos))
+            .catch(error => console.log(error))
     }
 
     useEffect(() => {
-        const requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-        };
-
-        fetch(
-            "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCmBr_x4gehHuQSL3d2OGB67vAeDA8QCVU",
-            requestOptions
-        )
-            .then(response => response.json())
-            .then(result => setVideos(result.items))
-            .catch(error => console.log('error', error));
+        youtube
+            .mostPopular()
+            .then(videos => setVideos(videos))
+            .catch(error => console.log(error))
     }, []);
 
     return (

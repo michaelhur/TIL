0612e366cf291ae7ece69252b82c1e6625290f13
michaelhur <jhhur@uwaterloo.ca>
2022-06-08 00:40:43 +0900

    ADD: returning to list

--- react/dream-coding/react-basic/youtube-clone/src/app.jsx
+++ react/dream-coding/react-basic/youtube-clone/src/app.jsx
@@ -2,14 +2,23 @@ import styles from './app.module.css';
 import { useState, useEffect } from 'react';
 import VideoList from "./components/video_list/video_list";
 import SearchHeader from "./components/search_header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
 
 function App({ youtube }) {
     const [videos, setVideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+
+    const selectVideo = (video) => {
+        setSelectedVideo(video);
+    }
 
     const onSearch = (query) => {
         youtube
             .search(query)
-            .then(videos => setVideos(videos))
+            .then(videos => {
+                setVideos(videos);
+                setSelectedVideo(null);
+            })
             .catch(error => console.log(error))
     }
 
@@ -23,7 +32,20 @@ function App({ youtube }) {
     return (
         <div className={styles.app}>
             <SearchHeader onSearch={onSearch} />
-            <VideoList videos={videos}/>
+            <div className={styles.content}>
+                {selectedVideo && (
+                    <div className={styles.detail}>
+                        <VideoDetail video={selectedVideo}/>
+                    </div>
+                )}
+                <div className={styles.list}>
+                    <VideoList
+                        videos={videos}
+                        onVideoClick={selectVideo}
+                        display={ selectedVideo ? 'list' : 'grid'}
+                    />
+                </div>
+            </div>
         </div>
     );
 }

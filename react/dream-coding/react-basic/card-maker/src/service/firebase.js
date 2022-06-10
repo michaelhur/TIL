ea82d379b5152393689971bd9a0b3e97ff738359
michaelhur <jhhur@uwaterloo.ca>
2022-06-11 00:50:42 +0900

    ADD: firebase

---
+++ react/dream-coding/react-basic/card-maker/src/service/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCeSNBtGlTA3aAPmjAREDXdisiYqIVGqho",
+    authDomain: "business-card-maker-dbf3e.firebaseapp.com",
+    databaseURL: "https://business-card-maker-dbf3e-default-rtdb.firebaseio.com",
+    projectId: "business-card-maker-dbf3e",
+    appId: "1:412498919753:web:209141ec38c88512341a98"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

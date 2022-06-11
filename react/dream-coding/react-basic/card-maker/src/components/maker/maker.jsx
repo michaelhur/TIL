e49055fef4716e7d33958628d79f73337bf8e55c
michaelhur <jhhur@uwaterloo.ca>
2022-06-11 23:54:32 +0900

    ADD: card editor and card preview component

--- react/dream-coding/react-basic/card-maker/src/components/maker/maker.jsx
+++ react/dream-coding/react-basic/card-maker/src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from "./maker.module.css"
 import {useLocation, useNavigate} from "react-router-dom";
 import Header from "../header/header";
@@ -7,6 +7,41 @@ import Editor from "../editor/editor";
 import Preview from "../preview/preview";
 
 const Maker = ({authService}) => {
+    const [cards, setCards] = useState([
+        {
+            id: "1",
+            name: "Elle",
+            company: "Samsung",
+            theme: "light",
+            title: "Software Engineer",
+            email: "ellie@gmail.com",
+            message: "go for it",
+            fileName: "ellie",
+            fileURL: "ellie.png"
+        },
+        {
+            id: "2",
+            name: "Mike",
+            company: "LinkStarter",
+            theme: "light",
+            title: "Software Engineer",
+            email: "mike.hur@gmail.com",
+            message: "go for it",
+            fileName: "mike",
+            fileURL: "mike.png"
+        },
+        {
+            id: "3",
+            name: "Son",
+            company: "Tottenham",
+            theme: "light",
+            title: "Football Player",
+            email: "son@gmail.com",
+            message: "Son for the Team of the Year",
+            fileName: "son",
+            fileURL: "son.png"
+        }
+    ])
     const { state } = useLocation();
     const navigate = useNavigate();
     const onLogout = () => {
@@ -25,8 +60,8 @@ const Maker = ({authService}) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout}/>
             <div className={styles.container}>
-                <Editor />
-                <Preview />
+                <Editor cards = {cards}/>
+                <Preview cards = {cards}/>
             </div>
             <Footer />
         </section>
--- react/dream-coding/react-basic/card-maker/src/components/preview/preview.jsx
+++ react/dream-coding/react-basic/card-maker/src/components/preview/preview.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import styles from './preview.module.css'
+import Card from "../card/card";
 
-const Preview = (props) => {
+const Preview = ({cards}) => {
     return (
         <section className={styles.preview}>
             <h1 className={styles.title}>Card Preview</h1>
+            {
+                cards.map(card => (
+                    <Card card={card} key={card.id}/>
+                ))
+            }
         </section>
     );
 }

0b051b855e8a9e7d8ead873b9c4ba9aef74370e9
michaelhur <jhhur@uwaterloo.ca>
2022-06-12 23:59:53 +0900

    ADD: FileInput as props

--- react/dream-coding/react-basic/card-maker/src/components/maker/maker.jsx
+++ react/dream-coding/react-basic/card-maker/src/components/maker/maker.jsx
@@ -6,7 +6,7 @@ import Footer from "../footer/footer";
 import Editor from "../editor/editor";
 import Preview from "../preview/preview";
 
-const Maker = ({authService}) => {
+const Maker = ({FileInput, authService}) => {
     const [cards, setCards] = useState({
         "1": {
             id: "1",
@@ -87,6 +87,7 @@ const Maker = ({authService}) => {
             <Header onLogout={onLogout}/>
             <div className={styles.container}>
                 <Editor
+                    FileInput={FileInput}
                     cards={cards}
                     addCard={addCard}
                     updateCard={updateCard}
